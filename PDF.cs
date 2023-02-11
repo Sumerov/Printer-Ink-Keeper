d41954180a571f3a2a5342e4b1f4d9fba093262c
Sumerov <bostlemen205@gmail.com>
2023-02-11 19:55:20 +0100

    V1.0

---
+++ PDF.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace InkKeeper
+{
+    public class PDF
+    {
+        public static Boolean PrintPDFs(string pdfFileName)
+        {
+            try
+            {
+                Process proc = new Process();
+                proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                proc.StartInfo.Verb = "print";
+
+                //Define location of adobe reader/command line
+                //switches to launch adobe in "print" mode
+                proc.StartInfo.FileName =
+                  @"C:\Program Files\Adobe\Acrobat DC\Acrobat\Acrobat.exe";
+                proc.StartInfo.Arguments = String.Format(@"/p /h {0}", pdfFileName);
+                proc.StartInfo.UseShellExecute = false;
+                proc.StartInfo.CreateNoWindow = true;
+
+                proc.Start();
+                proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                if (proc.HasExited == false)
+                {
+                    proc.WaitForExit(10000);
+                }
+
+                proc.EnableRaisingEvents = true;
+
+                proc.Close();
+                KillAdobe("AcroRd32");
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        //For whatever reason, sometimes adobe likes to be a stage 5 clinger.
+        //So here we kill it with fire.
+        private static bool KillAdobe(string name)
+        {
+            foreach (Process clsProcess in Process.GetProcesses().Where(
+                         clsProcess => clsProcess.ProcessName.StartsWith(name)))
+            {
+                clsProcess.Kill();
+                return true;
+            }
+            return false;
+        }
+    }//END Class
+}
---
+++ Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+namespace InkKeeper
+{
+    class inkKeeper {
+        private string configPath;
+        private DateTime lastDate;
+        private DateTime currentDate;
+        private int interval;
+
+        public inkKeeper() {
+            this.configPath = "config.txt";
+            this.currentDate = DateTime.Now;
+            try
+            {
+                string[] lines = File.ReadAllLines(this.configPath);
+                string str = "";
+                foreach(string line in lines)
+                {
+                    Console.WriteLine(line);
+                    string[] subs = line.Split('=');
+                    string variable = subs[0];
+                    string value = subs[1];
+                    switch (variable)
+                    {
+                        case "interval":
+                            this.interval = int.Parse(value);
+                            break;
+                        case "lastDate":
+                            this.lastDate = DateTime.Parse(value);
+                            break;
+                    }
+                }
+            } catch (Exception e)
+            {
+                string type = e.GetType().ToString();
+                Console.WriteLine(type);
+                this.lastDate = DateTime.Now;
+                if(type == "System.IO.FileNotFoundException") Console.WriteLine("File was not found, creating...");
+                if (type == "System.FormatException") Console.WriteLine("File has incorrect format, creating new one...");
+                this.interval = 5;
+                this.writeData();
+            }
+        }
+
+        public void writeData() 
+        {
+            string str = "lastDate= " + this.currentDate.ToString() + "\ninterval= " + this.interval.ToString();
+            File.WriteAllText(configPath, str); 
+        }
+        public string returnPath() { return this.configPath; }
+        public int returnInterval() { return this.interval; }
+        public DateTime returnCurrentDate() { return this.currentDate; }
+        public DateTime returnLastDate() { return this.lastDate; }
+        public TimeSpan compareDates() { return this.currentDate.Subtract(this.lastDate); }
+
+        public bool compareThreshold(int threshold)
+        {
+            if (this.compareDates() > TimeSpan.FromDays(threshold)) return true;
+            return false;
+        }
+        public void debugAddDays(double days) { this.currentDate = this.currentDate.AddDays(days); }
+        public bool checkInkDecay(int days)
+        {
+            bool shouldIprint = this.compareThreshold(days);
+            if(shouldIprint)
+            {
+                writeData();
+                string strExeFilePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+                string strWorkPath = System.IO.Path.GetDirectoryName(strExeFilePath) + "\\PDF";
+                string[] files = Directory.GetFiles(@strWorkPath);
+                foreach (string file in files.Where(
+                            file => file.ToUpper().Contains(".PDF")))
+                {
+                    Console.WriteLine("priting:" + file);
+                    PDF.PrintPDFs(file);
+                }
+                return true;
+            }
+            return false;
+        }
+
+    }
+    
+    class Program {
+        static void Main(string[] args)
+        {
+            inkKeeper keeper = new inkKeeper();
+            int days = keeper.returnInterval();
+            Console.WriteLine(" \n_____________________________________________\n");
+            Console.WriteLine("Ink keeper script! Current interval: " + days.ToString()) ;
+            Console.WriteLine("_____________________________________________\n");
+            if (!keeper.checkInkDecay(days)) {
+                Console.WriteLine("Interval not reached yet");
+                Console.WriteLine("Next print: " + keeper.returnLastDate().AddDays(days).ToString());
+            }
+            Console.WriteLine("_____________________________________________ \n");
+            Console.WriteLine("Good bye!");
+
+            /* Debug section */
+            /*
+            bool debug = true;
+            string reads;
+            while (debug == true)
+            {
+                try
+                {
+                    Console.WriteLine("Number OR Y|YES|0");
+                    reads = Console.ReadLine();
+                    if (reads == "Y" || reads == "YES" || reads == "0") debug = false;
+                    else keeper.debugAddDays(double.Parse(reads));
+                } catch (Exception e)
+                {
+                    Console.WriteLine("Incorrect number, skipping...");
+                }
+
+                Console.WriteLine("Current date:" + keeper.returnCurrentDate().ToString());
+                Console.WriteLine("Last date:" + keeper.returnLastDate().ToString());
+                Console.WriteLine("differences:" + keeper.compareDates());
+                Console.WriteLine("Threshold?:" + keeper.compareThreshold(4));
+            }
+            */
+            Thread.Sleep(5000);
+        }
+    }
+}
